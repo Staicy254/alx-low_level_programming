19d06f6f83ff997a30da252833bb3793b103021c
Staicy254 <jeanstaicy21@gmail.com>
2023-06-15 03:25:50 -0700

    Chnaging printf to puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * Description: main- prints out a string stdout
+ * Return: 0 i(success.
+ */
+{
+Int main (void)
+	puts("\"Programming is like building a multilingual puzzle");
+Return (0);
+}
