e815aba8ac19bfed495c8000df6012b8620b6fb0
Staicy254 <jeanstaicy21@gmail.com>
2023-06-16 14:58:15 -0700

    Random number

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * Determine if n = +ve number OR -ve number OR zero
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes here*/
+	if (n > 0)
+	{
+		printf("%d is %s\n", n, "positive");
+	}
+	else if (n < 0)
+	{
+		printf("%d is %s\n", n, "negative");
+        }
+	else
+	{
+		printf("%d is %s\n", n, "zero");
+	}
+	return (0);
+}
