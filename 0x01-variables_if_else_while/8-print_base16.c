7e49fde973b5e40c706088f89b9c6a73c45c51c4
Staicy254 <jeanstaicy21@gmail.com>
2023-06-19 15:24:22 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ */
+
+int main(void)
+{
+int w;
+
+for (w = 48; w < 58; w++)
+{
+putchar(w);
+}
+for (w = 97; w < 103; w++)
+{
+putchar(w);
+}
+putchar('\n');
+return (0);
+}
