cd13cfd78e2b3e66429f93d6bae10a12c7452247
Staicy254 <jeanstaicy21@gmail.com>
2023-06-19 13:44:33 -0700

    5Numerals

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main  - Entry point
+ *
+ * Return: Always 0 (succes)
+ */
+
+int main(void)
+{
+int number;
+while (number < 10)
+{
+printf("%d", number);
+number += 1;
+}
+printf("\n");
+return (0);
+}
+
